5729c85573f334efcbc624bdf8f659e8ff0e76ec
Ng-Levis-HungPhu <hungphunguyen.3103@gmail.com>
2025-07-07 09:37:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,231 +1,231 @@
-// const canvas = document.getElementById("rocketCanvas");
-// canvas.width = 1200;
-// canvas.height = 400;
+const canvas = document.getElementById("rocketCanvas");
+canvas.width = 1200;
+canvas.height = 400;
 
-// const aspect = canvas.width / canvas.height;
-// const zoom = 1.75;
-// const camera = new THREE.OrthographicCamera(
-//   -canvas.width / zoom, canvas.width / zoom,
-//   canvas.height / zoom, -canvas.height / zoom,
-//   1, 2000
-// );
-// camera.position.set(400, 100, 500);
-// camera.lookAt(0, 0, 0);
+const aspect = canvas.width / canvas.height;
+const zoom = 1.75;
+const camera = new THREE.OrthographicCamera(
+  -canvas.width / zoom, canvas.width / zoom,
+  canvas.height / zoom, -canvas.height / zoom,
+  1, 2000
+);
+camera.position.set(400, 100, 500);
+camera.lookAt(0, 0, 0);
 
-// const scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
-// const light = new THREE.PointLight(0xffffff, 1.2);
-// camera.add(light);
-// scene.add(camera);
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
-// scene.add(ambientLight);
+const light = new THREE.PointLight(0xffffff, 1.2);
+camera.add(light);
+scene.add(camera);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+scene.add(ambientLight);
 
-// const backLight = new THREE.DirectionalLight(0x8888ff, 0.4);
-// backLight.position.set(-100, -100, -200);
-// scene.add(backLight);
+const backLight = new THREE.DirectionalLight(0x8888ff, 0.4);
+backLight.position.set(-100, -100, -200);
+scene.add(backLight);
 
-// const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
-// renderer.setSize(canvas.width, canvas.height);
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+renderer.setSize(canvas.width, canvas.height);
 
-// const controls = new THREE.OrbitControls(camera, renderer.domElement);
-// controls.target.set(0, 0, 0);
-// controls.enableDamping = true;
-// controls.dampingFactor = 0.1;
-// controls.rotateSpeed = 0.5;
-// controls.zoomSpeed = 0.5;
-// controls.enablePan = false;
-// controls.update();
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+controls.enableDamping = true;
+controls.dampingFactor = 0.1;
+controls.rotateSpeed = 0.5;
+controls.zoomSpeed = 0.5;
+controls.enablePan = false;
+controls.update();
 
-// const axesHelper = new THREE.AxesHelper(100);
-// axesHelper.position.set(0, 50, 0);
-// scene.add(axesHelper);
-// const boxGeometry = new THREE.BoxGeometry(10, 10, 10);
-// const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-// const redBox = new THREE.Mesh(boxGeometry, boxMaterial);
-// redBox.position.set(0, 50, 0);
-// scene.add(redBox);
+const axesHelper = new THREE.AxesHelper(100);
+axesHelper.position.set(0, 50, 0);
+scene.add(axesHelper);
+const boxGeometry = new THREE.BoxGeometry(10, 10, 10);
+const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+const redBox = new THREE.Mesh(boxGeometry, boxMaterial);
+redBox.position.set(0, 50, 0);
+scene.add(redBox);
 
 
-// let rocketMesh = null;
-// let rocketGroup = new THREE.Group();
-// scene.add(rocketGroup);
+let rocketMesh = null;
+let rocketGroup = new THREE.Group();
+scene.add(rocketGroup);
 
-// function createWing(root, tip, height) {
-//   const shape = new THREE.Shape();
+function createWing(root, tip, height) {
+  const shape = new THREE.Shape();
  
-//   shape.moveTo(0, 0);
-//   shape.lineTo(root, 0);
-//   shape.lineTo(tip, height);
-//   shape.lineTo(0, height);
-//   shape.lineTo(0, 0);
+  shape.moveTo(0, 0);
+  shape.lineTo(root, 0);
+  shape.lineTo(tip, height);
+  shape.lineTo(0, height);
+  shape.lineTo(0, 0);
 
-//   shape.autoClose = true;
+  shape.autoClose = true;
 
-//   const extrudeSettings = { depth: 0.75, bevelEnabled: false};
-//   const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-//   const material = new THREE.MeshPhysicalMaterial({ color: 0x999999});
-//   return new THREE.Mesh(geometry, material);
-// }
+  const extrudeSettings = { depth: 0.75, bevelEnabled: false};
+  const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+  const material = new THREE.MeshPhysicalMaterial({ color: 0x999999});
+  return new THREE.Mesh(geometry, material);
+}
 
-// function createFin1(root, tip, height, swept) {
-//   const shape = new THREE.Shape();
+function createFin1(root, tip, height, swept) {
+  const shape = new THREE.Shape();
 
-//   const x1 = Math.tan(swept * Math.PI/180)
-//   const x2 = Math.tan(16.54 * Math.PI/180)
+  const x1 = Math.tan(swept * Math.PI/180)
+  const x2 = Math.tan(16.54 * Math.PI/180)
  
-//   shape.moveTo(0, 0);
-//   shape.lineTo(root, 0);
-//   shape.lineTo(tip - height*(x1 - x2), height);
-//   shape.lineTo(- height*(x1 - x2), height);
-//   shape.lineTo(0, 0);
+  shape.moveTo(0, 0);
+  shape.lineTo(root, 0);
+  shape.lineTo(tip - height*(x1 - x2), height);
+  shape.lineTo(- height*(x1 - x2), height);
+  shape.lineTo(0, 0);
 
-//   shape.autoClose = true;
+  shape.autoClose = true;
 
-//   const extrudeSettings = { depth: 0.75, bevelEnabled: false };
-//   const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-//   const material = new THREE.MeshPhysicalMaterial({ color: 0x666666 });
-//   return new THREE.Mesh(geometry, material);
-// }
+  const extrudeSettings = { depth: 0.75, bevelEnabled: false };
+  const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+  const material = new THREE.MeshPhysicalMaterial({ color: 0x666666 });
+  return new THREE.Mesh(geometry, material);
+}
 
-// function createFin2(root, tip, height, swept) {
-//   const shape = new THREE.Shape();
+function createFin2(root, tip, height, swept) {
+  const shape = new THREE.Shape();
  
-//   shape.moveTo(0, 0);
-//   shape.lineTo(root, 0);
-//   shape.lineTo(tip, height);
-//   shape.lineTo(0, height);
-//   shape.lineTo(0, 0);
+  shape.moveTo(0, 0);
+  shape.lineTo(root, 0);
+  shape.lineTo(tip, height);
+  shape.lineTo(0, height);
+  shape.lineTo(0, 0);
 
-//   shape.autoClose = true;
+  shape.autoClose = true;
 
-//   const extrudeSettings = { depth: 0.75, bevelEnabled: false };
-//   const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-//   const material = new THREE.MeshPhysicalMaterial({ color: 0x666666 });
-//   return new THREE.Mesh(geometry, material);
-// }
+  const extrudeSettings = { depth: 0.75, bevelEnabled: false };
+  const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+  const material = new THREE.MeshPhysicalMaterial({ color: 0x666666 });
+  return new THREE.Mesh(geometry, material);
+}
 
-// function createRocketModel() {
-//   const cssVars = getComputedStyle(document.body);
+function createRocketModel() {
+  const cssVars = getComputedStyle(document.body);
 
-//   const diameter = parseFloat(cssVars.getPropertyValue('--variable-collection-diameter'));
-//   const length = parseFloat(cssVars.getPropertyValue('--variable-collection-length'));
+  const diameter = parseFloat(cssVars.getPropertyValue('--variable-collection-diameter'));
+  const length = parseFloat(cssVars.getPropertyValue('--variable-collection-length'));
 
-//   const ln = parseFloat(document.getElementById('lnInput').value);
-//   const swept = parseFloat(document.getElementById('sweptInput').value);
+  const ln = parseFloat(document.getElementById('lnInput').value);
+  const swept = parseFloat(document.getElementById('sweptInput').value);
 
-//   const lw = parseFloat(cssVars.getPropertyValue('--variable-collection-LW'));
-//   const crw = parseFloat(cssVars.getPropertyValue('--variable-collection-CRW'));
-//   const ctw = parseFloat(cssVars.getPropertyValue('--variable-collection-CTW'));
-//   const bw = parseFloat(cssVars.getPropertyValue('--variable-collection-BW'));
+  const lw = parseFloat(cssVars.getPropertyValue('--variable-collection-LW'));
+  const crw = parseFloat(cssVars.getPropertyValue('--variable-collection-CRW'));
+  const ctw = parseFloat(cssVars.getPropertyValue('--variable-collection-CTW'));
+  const bw = parseFloat(cssVars.getPropertyValue('--variable-collection-BW'));
 
-//   const lf = parseFloat(cssVars.getPropertyValue('--variable-collection-LF'));
-//   const crf = parseFloat(cssVars.getPropertyValue('--variable-collection-CRF'));
-//   const ctf = parseFloat(cssVars.getPropertyValue('--variable-collection-CTF'));
-//   const bf = parseFloat(cssVars.getPropertyValue('--variable-collection-BF'));
+  const lf = parseFloat(cssVars.getPropertyValue('--variable-collection-LF'));
+  const crf = parseFloat(cssVars.getPropertyValue('--variable-collection-CRF'));
+  const ctf = parseFloat(cssVars.getPropertyValue('--variable-collection-CTF'));
+  const bf = parseFloat(cssVars.getPropertyValue('--variable-collection-BF'));
 
-//   const mode = document.getElementById('dropdownBtn').textContent.split(": ")[1];
+  const mode = document.getElementById('dropdownBtn').textContent.split(": ")[1];
 
-//   const bodyLength = length - ln;
+  const bodyLength = length - ln;
 
-//   const scaledDiameter = diameter * 10;
-//   const scaledBodyLength = bodyLength * 10;
-//   const scaledNoseLength = ln * 10;
+  const scaledDiameter = diameter * 10;
+  const scaledBodyLength = bodyLength * 10;
+  const scaledNoseLength = ln * 10;
 
-//   const scaledLW = lw * 10;
-//   const scaledCRW = crw * 10;
-//   const scaledCTW = ctw * 10;
-//   const scaledBW = bw * 10;
+  const scaledLW = lw * 10;
+  const scaledCRW = crw * 10;
+  const scaledCTW = ctw * 10;
+  const scaledBW = bw * 10;
 
-//   const scaledLF = lf * 10;
-//   const scaledCRF = crf * 10;
-//   const scaledCTF = ctf * 10;
-//   const scaledBF = bf * 10;
+  const scaledLF = lf * 10;
+  const scaledCRF = crf * 10;
+  const scaledCTF = ctf * 10;
+  const scaledBF = bf * 10;
 
 
-//   if (!rocketGroup) {
-//     rocketGroup = new THREE.Group();
-//     scene.add(rocketGroup);
-//   } else {
-//     rocketGroup.clear();
-//   }
+  if (!rocketGroup) {
+    rocketGroup = new THREE.Group();
+    scene.add(rocketGroup);
+  } else {
+    rocketGroup.clear();
+  }
 
-//   // === THÂN TÊN LỬA ===
-//   const bodyGeometry = new THREE.CylinderGeometry(
-//     scaledDiameter / 2,
-//     scaledDiameter / 2,
-//     scaledBodyLength,
-//     32
-//   );
-//   const bodyMaterial = new THREE.MeshPhysicalMaterial({ color: 0x00ccff });
-//   const bodyMesh = new THREE.Mesh(bodyGeometry, bodyMaterial);
-//   bodyMesh.rotation.set(0, 0, Math.PI / 2);
-//   bodyMesh.position.set(scaledBodyLength/2, 0, 0);
-//   rocketGroup.add(bodyMesh);
+  // === THÂN TÊN LỬA ===
+  const bodyGeometry = new THREE.CylinderGeometry(
+    scaledDiameter / 2,
+    scaledDiameter / 2,
+    scaledBodyLength,
+    32
+  );
+  const bodyMaterial = new THREE.MeshPhysicalMaterial({ color: 0x00ccff });
+  const bodyMesh = new THREE.Mesh(bodyGeometry, bodyMaterial);
+  bodyMesh.rotation.set(0, 0, Math.PI / 2);
+  bodyMesh.position.set(scaledBodyLength/2, 0, 0);
+  rocketGroup.add(bodyMesh);
 
-//   // === MŨI OGIVE ===
-//   const points = [];
-//   const segments = 50;
-//   const noseRadius = scaledDiameter / 2;
-//   for (let i = 0; i <= segments; i++) {
-//     const t = i / segments;
-//     const x = noseRadius * Math.sqrt(1 - Math.pow(t - 1, 2));
-//     const y = t * scaledNoseLength;
-//     points.push(new THREE.Vector2(x, y));
-//   }
+  // === MŨI OGIVE ===
+  const points = [];
+  const segments = 50;
+  const noseRadius = scaledDiameter / 2;
+  for (let i = 0; i <= segments; i++) {
+    const t = i / segments;
+    const x = noseRadius * Math.sqrt(1 - Math.pow(t - 1, 2));
+    const y = t * scaledNoseLength;
+    points.push(new THREE.Vector2(x, y));
+  }
 
-//   const noseGeometry = new THREE.LatheGeometry(points, 64);
-//   const noseMaterial = new THREE.MeshPhysicalMaterial({ color: 0x0077be });
-//   const noseMesh = new THREE.Mesh(noseGeometry, noseMaterial);
-//   noseMesh.rotation.set(0, 0, Math.PI / 2);
-//   noseMesh.position.set(scaledNoseLength+scaledBodyLength, 0, 0);
-//   rocketGroup.add(noseMesh);
+  const noseGeometry = new THREE.LatheGeometry(points, 64);
+  const noseMaterial = new THREE.MeshPhysicalMaterial({ color: 0x0077be });
+  const noseMesh = new THREE.Mesh(noseGeometry, noseMaterial);
+  noseMesh.rotation.set(0, 0, Math.PI / 2);
+  noseMesh.position.set(scaledNoseLength+scaledBodyLength, 0, 0);
+  rocketGroup.add(noseMesh);
 
-//   // === GẮN 1 CANARD LÊN THÂN ===
-//   const wingX = scaledBodyLength+scaledNoseLength-scaledCRW-scaledLW;
-//   const wingDistance = scaledDiameter / 2;
+  // === GẮN 1 CANARD LÊN THÂN ===
+  const wingX = scaledBodyLength+scaledNoseLength-scaledCRW-scaledLW;
+  const wingDistance = scaledDiameter / 2;
   
-//   const wingOffsets = [
-//     { y: wingDistance, z: 0 },
-//     { y: 0, z:  wingDistance },
-//     { y: -wingDistance, z: 0 },
-//     { y: 0, z: -wingDistance }
-//   ];
+  const wingOffsets = [
+    { y: wingDistance, z: 0 },
+    { y: 0, z:  wingDistance },
+    { y: -wingDistance, z: 0 },
+    { y: 0, z: -wingDistance }
+  ];
   
-//   wingOffsets.forEach((offset, index) => {
-//     const wing = createWing(scaledCRW, scaledCTW, scaledBW);
-//     wing.rotation.set(index * Math.PI / 2, 0, 0);
-//     wing.position.set(wingX, offset.y, offset.z);
-//     rocketGroup.add(wing);
-//   });
-//   // Gắn FIN
-//   const finX = scaledBodyLength+scaledNoseLength-scaledCRF-scaledLF;
-//   const finDistance = scaledDiameter / 2;
+  wingOffsets.forEach((offset, index) => {
+    const wing = createWing(scaledCRW, scaledCTW, scaledBW);
+    wing.rotation.set(index * Math.PI / 2, 0, 0);
+    wing.position.set(wingX, offset.y, offset.z);
+    rocketGroup.add(wing);
+  });
+  // Gắn FIN
+  const finX = scaledBodyLength+scaledNoseLength-scaledCRF-scaledLF;
+  const finDistance = scaledDiameter / 2;
   
-//   const finOffsets = [
-//     { y: finDistance, z: 0 },
-//     { y: 0, z:  finDistance },
-//     { y: -finDistance, z: 0 },
-//     { y: 0, z: -finDistance }
-//   ];
+  const finOffsets = [
+    { y: finDistance, z: 0 },
+    { y: 0, z:  finDistance },
+    { y: -finDistance, z: 0 },
+    { y: 0, z: -finDistance }
+  ];
   
-//   let createFinFunc;
+  let createFinFunc;
 
-//   if (mode === "NASA") {
-//     createFinFunc = createFin1;
-//   } else {
-//     createFinFunc = createFin2;
-//   }
+  if (mode === "NASA") {
+    createFinFunc = createFin1;
+  } else {
+    createFinFunc = createFin2;
+  }
 
-//   finOffsets.forEach((offset, index) => {
-//     const fin = createFinFunc(scaledCRF, scaledCTF, scaledBF, swept);
-//     fin.rotation.set(index * Math.PI / 2, 0, 0);
-//     fin.position.set(finX, offset.y, offset.z);
-//     rocketGroup.add(fin);
-//   });
+  finOffsets.forEach((offset, index) => {
+    const fin = createFinFunc(scaledCRF, scaledCTF, scaledBF, swept);
+    fin.rotation.set(index * Math.PI / 2, 0, 0);
+    fin.position.set(finX, offset.y, offset.z);
+    rocketGroup.add(fin);
+  });
 
-//   // === CĂN MÔ HÌNH GIỮA KHUNG ===
-//   rocketGroup.position.set(-500, 0, 0);
-// }
+  // === CĂN MÔ HÌNH GIỮA KHUNG ===
+  rocketGroup.position.set(-500, 0, 0);
+}
 
 function updateBodyLength() {
   const cssVars = getComputedStyle(document.body);
